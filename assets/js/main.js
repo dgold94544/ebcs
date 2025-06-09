74a01fdbdf61c0d45ba023867718812caa786334
dgold94544 <82000500+dgold94544@users.noreply.github.com>
2025-06-08 18:27:50 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,9 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-
-script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
-
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
+document.head.appendChild(script);
 
 
 (function($) {
