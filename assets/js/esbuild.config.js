5281ffecf9af3650901a9723cc2559b7fdac9980
dgold94544 <82000500+dgold94544@users.noreply.github.com>
2025-06-18 16:31:48 -0700

    Update esbuild.config.js

--- assets/js/esbuild.config.js
+++ assets/js/esbuild.config.js
@@ -4,4 +4,6 @@ require('esbuild').build({
   bundle: true,
   outfile: 'dist/worker.js',
   external: ['@cloudflare/kv-asset-handler'],
+  format: 'esm',
+  target: 'es2022',
 }).catch(() => process.exit(1));
